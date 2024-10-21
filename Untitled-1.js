67d485af3dcf97167872784058dc3d9ba8cc3f2a
Jithish J <135303624+jithsg@users.noreply.github.com>
2024-10-22 01:41:07 +0800

    commit 2

---
+++ Untitled-1.js
@@ -0,0 +1,16 @@
+// console.log("%cHello World there","color:blue; font-size:40px");
+//this is a comment
+
+/* this is
+a multiline
+comment 
+*/
+
+/*this is a multiline comment in single line*/
+
+var petDog = "Rex";
+console.log(petDog);
+var petCat = "Pepper";
+console.log(petCat);
+console.log("my pet cat name is ",petCat);
+console.log("my pet dog name is", petDog);
