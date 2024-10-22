60e35b555545f1690e180108ca7a0fe8495f95df
Jithish J <135303624+jithsg@users.noreply.github.com>
2024-10-22 20:39:46 +0800

    commit 3

--- Untitled-1.js
+++ Untitled-1.js
@@ -8,9 +8,54 @@ comment
 
 /*this is a multiline comment in single line*/
 
-var petDog = "Rex";
-console.log(petDog);
-var petCat = "Pepper";
-console.log(petCat);
-console.log("my pet cat name is ",petCat);
-console.log("my pet dog name is", petDog);
+// var petDog = "Rex";
+// console.log(petDog);
+// var petCat = "Pepper";
+// console.log(petCat);
+// console.log("my pet cat name is "+ petCat);
+// console.log("my pet dog name is", petDog);
+// console.log(2+2);
+// console.log(2/2);
+// console.log(2*2);
+// console.log(2>2);
+
+// var currentTime = 17;
+
+// console.log(currentTime>10 && currentTime<18);
+
+// var result = 50;
+// if (result>40){
+//     console.log("you passed the test");
+// }
+// else{
+//     console.log("you failed the test");
+// }
+
+var place = "first";
+
+// if (place=="first"){
+//     console.log("gold");
+// }
+// else if (place=="second"){
+//     console.log("silver");
+// }
+// else if (place=="third"){
+//     console.log("bronze");
+// }
+// else{
+//     console.log("no medal");
+// }
+
+// switch(place){
+//     case  "first":
+//         console.log("gold");
+//         break;
+//     case "second":
+//         console.log("silver");
+//         break;
+//     case "third":
+//         console.log("bronze");
+//         break
+//     default:
+//         console.log("no medal");
+// }
