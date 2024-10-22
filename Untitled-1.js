c4c3511f4a513aa3d65724e442dc91623e29c0a1
Jithish J <135303624+jithsg@users.noreply.github.com>
2024-10-22 21:13:57 +0800

    commit 4

--- Untitled-1.js
+++ Untitled-1.js
@@ -31,7 +31,7 @@ comment
 //     console.log("you failed the test");
 // }
 
-var place = "first";
+// var place = "first";
 
 // if (place=="first"){
 //     console.log("gold");
@@ -59,3 +59,34 @@ var place = "first";
 //     default:
 //         console.log("no medal");
 // }
+
+
+// if (age>=65){
+//     console.log("You get your income from your pension")
+// }
+// else if (age<65 && age>=18){
+//     console.log("Each month you get a salary")
+// }
+// else if (age<18){
+//     console.log("You get an allowance")
+// }
+// else{
+//     console.log("The value of the age variable is not numerical")
+// }
+
+var age =10;
+switch(true){
+    case age>=65:
+        console.log("You get your income from your pension")
+        break;
+    case age<65 && age>=18:
+        console.log("Each month you get a salary")
+        break;
+    case age<18:
+        console.log("You get an allowance")
+        break;
+    default:
+        console.log("The value of the age variable is not numerical")
+        break
+
+}
